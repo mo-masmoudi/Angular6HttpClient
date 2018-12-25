2a205bc542ad73809269edc795aea9fc44a8ce65
DalyIng <mohammedali.masmoudi@gmail.com>
2018-12-25 04:24:49 +0100

    Deploy to Heroku

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/Angular6HTTPCLIENT'));
+app.use(express.static(__dirname + "/dist/Angular6"));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/Angular6HTTPCLIENT/index.html'));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/Angular6/index.html"));
 });
 
 // default Heroku port
